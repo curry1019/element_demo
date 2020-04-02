e595dadbd5e4988416324b77f04b70ea04f287b4
guomh <17611012203@163.com>
2020-04-02 18:08:31 +0800

    测试上传到git

---
+++ src/views/test1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        我是测试 push到git
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
